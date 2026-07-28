2f87b95c042f59effee482f1e4ad2264243fdaad
DkayMM <jairodkayx@gmail.com>
2026-07-28 20:16:53 +0200

    Minor typo fix

--- Frontend/src/pages/login.tsx
+++ Frontend/src/pages/login.tsx
@@ -94,9 +94,9 @@ export const Login = () => {
                             <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
                             <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
                         </svg>
-                        <p className="text-gray-800 dark:text-zinc-100 font-bold text-lg mb-2">Iniciando servidor...</p>
+                        <p className="text-gray-800 dark:text-zinc-100 font-bold text-lg mb-2">Initializing server...</p>
                         <p className="text-gray-600 dark:text-zinc-400 text-sm">
-                            Por favor, espera a que Render inicialice el servidor, esto llevará alrededor de 30 segundos.
+                            Please wait for the Render server to initialize, this can take up to 30 seconds.
                         </p>
                     </div>
                 </div>
